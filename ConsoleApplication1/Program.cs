3bbf0712b2e1f30a24e7de28a44e908bcb139002
anayk <anayk@A-KHAIRATKAR>
2016-12-27 13:08:50 -0800

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 2,34,56,7,8,33 };
+            int[] arr = {8,7, 6, 5, 4, 3, 2, 1 };
+            #region Selection sort 
+
+            int min, temp;
+            for (int i = 0; i < arr.Length -1; i++)
+            {
+                min = i;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if(arr[min] > arr[j])
+                    {
+                        min = j;
+                    }
+
+                }
+                temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+
+            }
+            #endregion
+
+
+            #region bubblesort
+
+
+
+
+            #endregion
+
+
+
+
+        }
+    }
+}
