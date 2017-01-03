97fc91d8cfb8988b02d368b9aca46d284b61cf0b
annuk <annuk@DESKTOP-RRPP5LP>
2017-01-02 23:23:25 -0800

    Added extra functions for binary tree

--- ConsoleApplication1/BinaryTree.cs
+++ ConsoleApplication1/BinaryTree.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     class BinaryTree
     {
 
-        BTNode root = null;
+        public BTNode root = null;
 
 
         public void find(int d)
@@ -51,7 +51,7 @@ namespace ConsoleApplication1
 
                     if (d < runner.data)
                     {
-                        
+
                         if (runner.left == null)
                         {
                             runner.left = newnode;
@@ -61,7 +61,7 @@ namespace ConsoleApplication1
                         {
                             runner = runner.left;
                         }
-                       
+
                     }
                     else
                     {
@@ -74,7 +74,7 @@ namespace ConsoleApplication1
                         {
                             runner = runner.right;
                         }
-                     
+
                     }
                 }
 
@@ -92,58 +92,7 @@ namespace ConsoleApplication1
             {
                 return;
             }
-            #region inorder
-            // traverse entire left sub tree
-            //while (runner != null)
-            //{
-            //    S.Push(runner);
-            //    runner = runner.left;
-            //}
-            ////Stop when left is null check if right node exists, go to it and traverse its left side. If both left and right and enpty pop and check the root
-            //while (S.Count > 0)
-            //{
-            //     runner = S.Pop();
-            //    Console.WriteLine(runner.data);
-            //    if (runner.right != null)
-            //    {
-            //        runner = runner.right;
-            //        while (runner != null)
-            //        {
-            //            S.Push(runner);
-            //            runner = runner.left;
-
-            //        }
-
-            //    }
-            //}
-            #endregion
-
-            #region preorder
-            // push the root into the stack 
-            //S.Push(runner);
-
-            //while (S.Count > 0)
-            //{   //pop the lement from the stack 
-            //    runner = S.Peek();
-            //    Console.WriteLine(runner.data);
-            //    S.Pop();
-            //    //first push right element into the stack then push the left element
-            //    if (runner.right != null)
-            //    {
-
-            //        S.Push(runner.right);
-            //    }
-            //    if (runner.left != null)
-            //    {
-
-            //        S.Push(runner.left);
-            //    }
-
-            //}
-
-            #endregion
-
-            #region postorder
+            #region inorder  traverse entire left sub tree
             while (runner != null)
             {
                 S.Push(runner);
@@ -166,11 +115,133 @@ namespace ConsoleApplication1
 
                 }
             }
+        }
+
+        #endregion
+
+        #region preorder
+        // push the root into the stack 
+        //S.Push(runner);
+
+        //while (S.Count > 0)
+        //{   //pop the lement from the stack 
+        //    runner = S.Peek();
+        //    Console.WriteLine(runner.data);
+        //    S.Pop();
+        //    //first push right element into the stack then push the left element
+        //    if (runner.right != null)
+        //    {
+
+        //        S.Push(runner.right);
+        //    }
+        //    if (runner.left != null)
+        //    {
+
+        //        S.Push(runner.left);
+        //    }
+
+        //}
+
+        #endregion
+
+        #region postorder
 
 
-            #endregion
+        #endregion
+
+        #region Level order traversal 
+        public void levelordertrav(BTNode root)
+        {
+            Queue<BTNode> q = new Queue<BTNode>();
+            BTNode runner = root;
+            q.Enqueue(root);
+
+
+            while (q.Count > 0)
+            {
+
+                BTNode newnode = q.Peek();
+                q.Dequeue();
+                Console.WriteLine(newnode.data);
+                if (newnode.left != null)
+                {
+                    q.Enqueue(newnode.left);
+                }
+                if (newnode.right != null)
+                {
+                    q.Enqueue(newnode.right);
+                }
+
+
+
+
+            }
+
+
+
+
+        }
+        #endregion
+
+
+
+        #region recvursive depth
+        public int FindDepth(BTNode r)
+        {
+            if (r == null)
+            {
+                return 0;
+            }
+            else
+            {
+                BTNode runner = r;
+                int leftside = FindDepth(runner.left);
+                int rightside = FindDepth(runner.right);
+                int height = leftside > rightside ? leftside : rightside;
+
+                return height + 1;
+            }
+        }
+        #endregion
+
+        #region iterative depth 
+        public int finddepthiter(BTNode r)
+        {
+            Queue<BTNode> q = new Queue<BTNode>();
+            BTNode runner = r;
+
+            int height = 0;
+            while (true)
+            {
+                q.Enqueue(r);
+                int nodecount = q.Count;
+                if (nodecount == 0)
+                {
+                    return height;
+                }
+                height++;
+                while (nodecount > 0)
+                {
+                    BTNode newnode = q.Peek();
+                    q.Dequeue();
+                    if (newnode.left != null)
+                    {
+                        q.Enqueue(newnode.left);
+                    }
+                    if (newnode.right != null)
+                    {
+                        q.Enqueue(newnode.right);
+                    }
+                    nodecount--;
+                }
+
+            }
+
 
         }
 
+
+
+        #endregion
     }
 }
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,9 +11,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int[] arr1 = { 2,34,56,7,8,33 };
-            int[] arr = {8,7, 6, 5, 4, 3, 2, 1 };
-            int temp;
+            int[] arr1 = { 2, 34, 56, 7, 8, 33 };
+            int[] arr = { 8, 7, 6, 5, 4, 3, 2, 1 };
+           
             //#region Selection sort 
 
             //int min;
@@ -36,7 +36,7 @@ namespace ConsoleApplication1
             //#endregion
 
 
-            //#region bubblesort
+            #region bubblesort
 
             //for (int i = 0; i < arr.Length; i++)
             //{
@@ -52,9 +52,35 @@ namespace ConsoleApplication1
             //}
 
 
-            //#endregion
+            #endregion
 
-            //#region Insertionsort
+            #region bubblesort sligtly optimized version 
+
+            //Boolean isSorted = false;
+            //int lastsorted = arr.Length - 1;
+            //while (!isSorted)
+            //{
+            //    isSorted = true;
+            //    for (int i = 0; i < lastsorted; i++)
+            //    {
+            //        if (arr[i] > arr[i + 1])
+            //        {
+            //            int tmp = arr[i];
+            //            arr[i] = arr[i + 1];
+            //            arr[i + 1] = tmp;
+            //            isSorted = false;
+            //        }
+
+            //    }
+            //    lastsorted--;
+
+            //}
+
+
+
+            #endregion
+
+            #region Insertionsort
             //int hole, val;
             //for (int i = 1; i < arr.Length; i++)
             //{
@@ -69,10 +95,14 @@ namespace ConsoleApplication1
 
             //}
 
-            //#endregion
+            #endregion
 
-            #region Mergesort
+            #region Divide and conquer algos
 
+            #region Mergesort  
+            //int p = 0;
+            //int q = arr.Length - 1;
+            //mergesort(arr, p, q);
 
 
             #endregion
@@ -80,7 +110,7 @@ namespace ConsoleApplication1
             #region Quicksort                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
             #endregion
 
-
+            #endregion
             //#region right rotation
             //int[] newa = new int[arr.Length];
             //Console.WriteLine("Enter a rotation ");
@@ -169,16 +199,103 @@ namespace ConsoleApplication1
 
 
             #region Binary tree 
+            #region depth of binary tree
 
+            #endregion
 
             BinaryTree T = new BinaryTree();
             T.InsertNode(55);
             T.InsertNode(33);
             T.InsertNode(91);
             T.InsertNode(26);
+
             T.DisplayBTree();
+            T.levelordertrav(T.root);
+            Console.WriteLine(T.FindDepth(T.root));
             #endregion
 
+
+
+            //char[] chararr = new char[26];
+
+            //List<char> listchar = new List<char>(26);
+
+            //string first = Console.ReadLine();
+            //string second = Console.ReadLine();
+            //int[] lettercounts = new int[26];
+            //foreach (char c in first.ToCharArray())
+            //{
+            //    lettercounts[c - 'a']++;
+            //}
+            //foreach (char c in second.ToCharArray())
+            //{
+            //    lettercounts[c - 'a']--;
+            //}
+            //int result = 0;
+            //foreach (int i in lettercounts)
+            //{
+            //    result += Math.Abs(i);
+            //}
+            #region stack brackets 
+            //    List<string> result = new List<string>();
+            //    int t = Convert.ToInt32(Console.ReadLine());
+            //    for (int a0 = 0; a0 < t; a0++)
+            //    {
+            //        string s = Console.ReadLine();
+            //        char[] chararr = s.ToCharArray();
+            //        Stack<char> charstack = new Stack<char>();
+            //        foreach (char c in chararr)
+            //        {
+
+
+            //            if (c == '{' || c == '(' || c == '[')
+            //                {
+            //                    charstack.Push(c);
+            //                }
+
+            //            else
+            //            {
+            //                if(charstack.Count == 0)
+            //                {
+            //                    break;
+            //                }
+            //                else
+            //                if (c == '}' || c == ')' || c == ']')
+            //                {
+            //                    char topchar = charstack.Peek();
+
+            //                    if(c=='}' && topchar=='{' || c==']' && topchar=='[' || c == ')' && topchar=='(')
+            //                    {
+            //                        charstack.Pop();
+            //                    }
+            //                }
+
+            //            }
+
+
+            //        }
+
+            //        if (charstack.Count == 0) result.Add("YES"); else result.Add("NO");
+            //    }
+
+            //    foreach (var item in result)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+
+            //}
+            #endregion
         }
+        public static void mergesort(int[] arr, int p, int q)
+        {
+            if (p < q)
+            {
+                int mid = (p + q) / 2;
+                mergesort(arr, p, mid);
+                mergesort(arr, mid + 1, q);
+               
+            }
+        }
+       
     }
 }
